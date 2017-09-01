ae361e50f0ec24a8ef6880a2196ee745d959beb7
Joao Neves <joaoneves792@gmail.com>
2017-09-01 11:41:17 +0100

    Updated compose file for latest docker release

--- helperservice.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-docker service create \
-	--name vis-helper \
-	--network host \
-	--mode global \
-	--mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
-	--constraint  'node.labels.master != true' \
-	warpenguin.no-ip.org/warpvisualizerhelper
